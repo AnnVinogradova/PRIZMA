5cf8189791013b38cb4ea0f2a307627c56f36a2d
AnnVinogradova <saikotakaki@gmail.com>
2023-06-03 11:44:45 +0300

    -----

--- experiments/1_create.mjs
+++ experiments/1_create.mjs
@@ -2,10 +2,22 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+async function main() {
+	const user = await prisma.user.create({
+		data: {
+			name: 'Alice',
+			email: 'alice@prisma.io',
+		},
+	});
+	console.log(user);
+}
 
-const newUser = await prisma.user.create({
-	data: {
-		name: 'Alice',
-		email: 'alice@prisma.io',
-	},
-})
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
--- experiments/2_getAll.mjs
+++ experiments/2_getAll.mjs
@@ -2,6 +2,19 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const users = await prisma.user.findMany();
+async function main() {
+	const users = await prisma.user.findMany({
+		include: { address: true, company: true }
+	});
+	console.log(users);
+}
 
-console.log(users);
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
